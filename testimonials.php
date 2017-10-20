f30ac7f7ff85082936f078e7e1248d9edd94dd74
Jason S <jjs180@gmail.com>
2017-10-20 11:56:00 +0000

    Added testimonials page

---
+++ testimonials.php
@@ -0,0 +1,210 @@
+<?php include_once('header.html'); ?>
+
+  <!-- Content -->
+  <div id="content"> 
+    
+    <!-- Company Presentation -->
+    <section class="presentation padding-top-70 padding-bottom-70">
+      <div class="container">
+        <div class="single-progect text-center">
+          <div class="row">
+            <div class="col-md-10 center-auto">
+              <div class="heading text-center"> <!--<span>April 6, 2017</span>-->
+                <h4>What our clients say</h4>
+              </div>
+              
+              <!-- Images -->
+              <!--
+              <ul class="row margin-bottom-50">
+                <li class="col-md-6 margin-bottom-30"> <img class="img-responsive" src="images/portfolio/img-1.jpg" alt="" > </li>
+                <li class="col-md-6 margin-bottom-30"> <img class="img-responsive" src="images/portfolio/img-2.jpg" alt="" > </li>
+                <li class="col-md-6 margin-bottom-30"> <img class="img-responsive" src="images/portfolio/img-3.jpg" alt="" > </li>
+                <li class="col-md-6 margin-bottom-30"> <img class="img-responsive" src="images/portfolio/img-4.jpg" alt="" > </li>
+              </ul>
+              -->
+              <div class="row">
+                <div class="col-md-8 text-left">
+                  <!--<h4 class="margin-bottom-40">THE PROJECT</h4>
+                  <p>Donec venenatis, turpis vel hendrerit interdum, dui ligula ultricies purus, sed posuere libero dui id orci. <br />
+                    Nam congue, pede vitae dapibus aliquet interdum dui ligula ultricies purus, sed posuere. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ea, nihil, dolores, culpa ullam vero ipsum placeat accusamus nemo ipsa cupiditate id molestiae consectetur quae pariatur repudiandae vel ex quaerat nam iusto aliquid laborum quia adipisci aut ut impedit obcaecati nisi deleniti tempore maxime. <br />
+                    Donec venenatis, turpis vel hendrerit interdum, dui ligula ultricies purus, sed posuere libero dui id orci. Nam congue, pede vitae dapibus aliquet interdum dui ligula ultricies purus, sed posuere. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ea, nihil, dolores, <br />
+                    <br />-->
+                    <blockquote><em>"Cloud Mine is a great consulting partner. They think ahead...they help you gather requirements for current projects, as well as making sure you are building out your Salesforce instance for the future. They saved me so much time with their custom applications!"</em> 
+                    <br/><br/>
+                    -Celia Suh<br/>
+                    Senior Salesforce Project Manager<br/>
+                    Nativo, Inc.
+                    </blockquote> </p>
+                </div>
+                <div class="col-md-4 text-left">
+                  <h4 class="margin-bottom-10">SUMMARY </h4>
+                  <!-- Project Info -->
+                  <ul class="project-info">
+                    <li>
+                      <p>Project</p>
+                      <span>Custom Salesforce Development</span> </li>
+                    <li>
+                      <p>Completed</p>
+                      <span>Ongoing</span> </li>
+                    <li>
+                      <p>technology</p>
+                      <span>APEX, Visualforce, REST APIs</span> </li>
+                    <li><a href="https://www.nativo.com/" class="btn btn-1 margin-bottom-100 margin-top-20">CLIENT WEBSITE <i class="fa fa-caret-right"></i></a></li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="single-progect text-center">
+          <div class="row">
+            <div class="col-md-10 center-auto">
+              <!-- Images -->
+              <!--
+              <ul class="row margin-bottom-50">
+                <li class="col-md-6 margin-bottom-30"> <img class="img-responsive" src="images/portfolio/img-1.jpg" alt="" > </li>
+                <li class="col-md-6 margin-bottom-30"> <img class="img-responsive" src="images/portfolio/img-2.jpg" alt="" > </li>
+                <li class="col-md-6 margin-bottom-30"> <img class="img-responsive" src="images/portfolio/img-3.jpg" alt="" > </li>
+                <li class="col-md-6 margin-bottom-30"> <img class="img-responsive" src="images/portfolio/img-4.jpg" alt="" > </li>
+              </ul>
+              -->
+              <div class="row">
+                <div class="col-md-8 text-left">
+                  <!--<h4 class="margin-bottom-40">THE PROJECT</h4>
+                  <p>Donec venenatis, turpis vel hendrerit interdum, dui ligula ultricies purus, sed posuere libero dui id orci. <br />
+                    Nam congue, pede vitae dapibus aliquet interdum dui ligula ultricies purus, sed posuere. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ea, nihil, dolores, culpa ullam vero ipsum placeat accusamus nemo ipsa cupiditate id molestiae consectetur quae pariatur repudiandae vel ex quaerat nam iusto aliquid laborum quia adipisci aut ut impedit obcaecati nisi deleniti tempore maxime. <br />
+                    Donec venenatis, turpis vel hendrerit interdum, dui ligula ultricies purus, sed posuere libero dui id orci. Nam congue, pede vitae dapibus aliquet interdum dui ligula ultricies purus, sed posuere. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ea, nihil, dolores, <br />
+                    <br />-->
+                    <blockquote><em>"Cloud Mine has been exceptional in providing feedback, scoping, and suggestion to architecting our new system. We have very specific business needs and they have addressed every obstacle with "no problem, we can do that" from building permission sets and profiles, to workflows and custom objects. They are responsive and always available. Very thankful to have chosen them as our preferred contractor. 10/10"</em>
+                    <br/><br/>
+                    -Andre Hinojosa<br/>
+                    Staff Accountant / Finance<br/>
+                    Doctor Evidence
+                    </blockquote> </p>
+                </div>
+                <div class="col-md-4 text-left">
+                  <h4 class="margin-bottom-10">SUMMARY </h4>
+                  <!-- Project Info -->
+                  <ul class="project-info">
+                    <li>
+                      <p>Project</p>
+                      <span>Salesforce Configuration and Development</span> </li>
+                    <li>
+                      <p>Completed</p>
+                      <span>2017</span> </li>
+                    <li>
+                      <p>Technology</p>
+                      <span>Salesforce Sales Cloud, APEX, Visualforce</span> </li>
+                    <li><a href="http://drevidence.com/" class="btn btn-1 margin-bottom-100 margin-top-20">CLIENT WEBSITE <i class="fa fa-caret-right"></i></a></li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="single-progect text-center">
+          <div class="row">
+            <div class="col-md-10 center-auto">
+              <!-- Images -->
+              <!--
+              <ul class="row margin-bottom-50">
+                <li class="col-md-6 margin-bottom-30"> <img class="img-responsive" src="images/portfolio/img-1.jpg" alt="" > </li>
+                <li class="col-md-6 margin-bottom-30"> <img class="img-responsive" src="images/portfolio/img-2.jpg" alt="" > </li>
+                <li class="col-md-6 margin-bottom-30"> <img class="img-responsive" src="images/portfolio/img-3.jpg" alt="" > </li>
+                <li class="col-md-6 margin-bottom-30"> <img class="img-responsive" src="images/portfolio/img-4.jpg" alt="" > </li>
+              </ul>
+              -->
+              <div class="row">
+                <div class="col-md-8 text-left">
+                  <!--<h4 class="margin-bottom-40">THE PROJECT</h4>
+                  <p>Donec venenatis, turpis vel hendrerit interdum, dui ligula ultricies purus, sed posuere libero dui id orci. <br />
+                    Nam congue, pede vitae dapibus aliquet interdum dui ligula ultricies purus, sed posuere. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ea, nihil, dolores, culpa ullam vero ipsum placeat accusamus nemo ipsa cupiditate id molestiae consectetur quae pariatur repudiandae vel ex quaerat nam iusto aliquid laborum quia adipisci aut ut impedit obcaecati nisi deleniti tempore maxime. <br />
+                    Donec venenatis, turpis vel hendrerit interdum, dui ligula ultricies purus, sed posuere libero dui id orci. Nam congue, pede vitae dapibus aliquet interdum dui ligula ultricies purus, sed posuere. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ea, nihil, dolores, <br />
+                    <br />-->
+                    <blockquote><em>"...Cloud Mine was professional, knowledgeable and delivered projects above our expectations prior to our deadlines. We look forward to a continued partnership with Cloud Mine Consulting."</em>
+                    <br/><br/>
+                    -Bret Patrick Fair<br/>
+                    Founder & Managing Partner<br/>
+                    360 Risk Partners
+                    </blockquote> </p>
+                </div>
+                <div class="col-md-4 text-left">
+                  <h4 class="margin-bottom-10">SUMMARY </h4>
+                  <!-- Project Info -->
+                  <ul class="project-info">
+                    <li>
+                      <p>Project</p>
+                      <span>Salesforce Configuration and Development</span> </li>
+                    <li>
+                      <p>Completed</p>
+                      <span>Ongoing</span> </li>
+                    <li>
+                      <p>Technology</p>
+                      <span>Salesforce Sales Cloud, APEX, Visualforce</span> </li>
+                    <li><a href="http://www.360riskpartners.com/" class="btn btn-1 margin-bottom-100 margin-top-20">CLIENT WEBSITE <i class="fa fa-caret-right"></i></a></li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="single-progect text-center">
+          <div class="row">
+            <div class="col-md-10 center-auto">
+              <!-- Images -->
+              <!--
+              <ul class="row margin-bottom-50">
+                <li class="col-md-6 margin-bottom-30"> <img class="img-responsive" src="images/portfolio/img-1.jpg" alt="" > </li>
+                <li class="col-md-6 margin-bottom-30"> <img class="img-responsive" src="images/portfolio/img-2.jpg" alt="" > </li>
+                <li class="col-md-6 margin-bottom-30"> <img class="img-responsive" src="images/portfolio/img-3.jpg" alt="" > </li>
+                <li class="col-md-6 margin-bottom-30"> <img class="img-responsive" src="images/portfolio/img-4.jpg" alt="" > </li>
+              </ul>
+              -->
+              <div class="row">
+                <div class="col-md-8 text-left">
+                  <!--<h4 class="margin-bottom-40">THE PROJECT</h4>
+                  <p>Donec venenatis, turpis vel hendrerit interdum, dui ligula ultricies purus, sed posuere libero dui id orci. <br />
+                    Nam congue, pede vitae dapibus aliquet interdum dui ligula ultricies purus, sed posuere. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ea, nihil, dolores, culpa ullam vero ipsum placeat accusamus nemo ipsa cupiditate id molestiae consectetur quae pariatur repudiandae vel ex quaerat nam iusto aliquid laborum quia adipisci aut ut impedit obcaecati nisi deleniti tempore maxime. <br />
+                    Donec venenatis, turpis vel hendrerit interdum, dui ligula ultricies purus, sed posuere libero dui id orci. Nam congue, pede vitae dapibus aliquet interdum dui ligula ultricies purus, sed posuere. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ea, nihil, dolores, <br />
+                    <br />-->
+                    <blockquote><em>"Very skilled and affordable solutions (i.e. good value for our needs without excessive prep work we didn't need.). Highly recommended."</em>
+                    <br/><br/>
+                    -Bill Jeffries<br/>
+                    SVP of Growth<br/> 
+                    Restoration Media
+                    </blockquote> </p>
+                </div>
+                <div class="col-md-4 text-left">
+                  <h4 class="margin-bottom-10">SUMMARY </h4>
+                  <!-- Project Info -->
+                  <ul class="project-info">
+                    <li>
+                      <p>Project</p>
+                      <span>Sales Cloud Configuration and Custom Invoicing</span> </li>
+                    <li>
+                      <p>Completed</p>
+                      <span>2016</span> </li>
+                    <li>
+                      <p>Technology</p>
+                      <span>Salesforce Sales Cloud, APEX, Visualforce</span> </li>
+                    <li><a href="http://restorationmedia.com/" class="btn btn-1 margin-bottom-100 margin-top-20">CLIENT WEBSITE <i class="fa fa-caret-right"></i></a></li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>        
+      </div>
+    </section>
+  </div>
+  
+<?php include('footer.html') ?>
+
+<script type="text/javascript">
+ $( document ).ready(function() {
+    $("li#nav_testimonials").attr('class', 'active');
+}); 
+</script>
+
+</body>
+</html>
